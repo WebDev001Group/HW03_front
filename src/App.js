c6dc1f8e201b85d514e3e498f7b6d98df1a592b3
yasin <yasinnmoosavi@gmail.com>
2021-12-28 00:54:39 +0330

    start up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PrivateRoute } from "./router/privateRoute";
+import { LoginPage } from "./components/loginPage";
+import { NotePage } from "./components/notePage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/notes" element={<PrivateRoute />}>
+          <Route path="/notes" element={<NotePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+// function App(props) {
+//   return (
+//     <div className="App">
+//       <div>Count: {props.count}</div>
+//       <button onClick={() => props.increaseCounter()}>Increase Count</button>
+//       <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.counter.count,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/loginPage.js
@@ -0,0 +1,102 @@
+import { Button, Card, Input, Row } from "antd";
+import Form from "antd/lib/form/Form";
+import { useState } from "react";
+
+export const LoginPage = () => {
+  return (
+    <div className="App">
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{ minHeight: "100vh" }}
+      >
+        <Card>
+          <Demo />
+        </Card>
+      </Row>
+    </div>
+  );
+};
+const Demo = () => {
+  const [type, setType] = useState(true);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      {type ? (
+        <Form.Item
+          label="Reapeat"
+          name="r_password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+      ) : null}
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+      <Button onClick={() => setType(!type)}>
+        {!type ? "go to sign in" : "go to log in"}
+      </Button>
+    </Form>
+  );
+};
